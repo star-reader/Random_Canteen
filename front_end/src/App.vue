687693c72c3d6d1db3004d4e72b9e57f0b5b0ed9
star-reader <3084895416@qq.com>
2024-11-05 19:38:17 +0800

    初始化导航栏

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -1,9 +1,9 @@
 <template>
-
+    <NavigationTabBar />
 </template>
 
 <script lang='ts' setup>
-
+import NavigationTabBar from './layout/NavBar/NavigationTabBar.vue';
 </script>
 
 <style lang='less' scoped>
--- front_end/src/layout/NavBar/NavigationTabBar.vue
+++ front_end/src/layout/NavBar/NavigationTabBar.vue
@@ -1,9 +1,17 @@
 <template>
-
+    <div class="app-nav-bar">
+        <van-tabbar v-model="active">
+        <van-tabbar-item name="food" icon="home-o">干饭</van-tabbar-item>
+        <van-tabbar-item name="explore" icon="location-o">探索</van-tabbar-item>
+        <van-tabbar-item name="moment" icon="chat-o">交流</van-tabbar-item>
+        <van-tabbar-item name="my" icon="friends-o">主页</van-tabbar-item>
+        </van-tabbar>
+    </div>
 </template>
 
 <script lang='ts' setup>
-
+import { ref } from 'vue'
+const active = ref('food')
 </script>
 
 <style lang='less' scoped>
