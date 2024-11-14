a7a9dced88d677b9b17d875975b0d94c5e9353d1
Asuna-afk <1991327927@qq.com>
2024-11-14 23:18:30 +0800

    Create ProfileHeader.vue
    个人主页的头像组件

---
+++ front_end/src/components/My/ProfileHeader.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="background">
+        <div class="profile">
+            <img src="../assets/food/pageBackground.jpg" class="head-portrait" alt="User Profile Picture">
+            <div class="name">用户名</div> <!-- 添加用户名 -->
+        </div>
+    </div>
+</template>
+
+<style lang="less" scoped>
+.background {
+    width: 100%; 
+    height: 25vh; 
+    background-image: url('../assets/food/backgound.jpg'); 
+    background-size: cover; 
+    background-position: center; 
+    display: flex; 
+    justify-content: flex-start;
+    align-items: center; 
+    padding: 10px;
+}
+
+.profile {
+    display: flex;
+    align-items: center; 
+}
+
+.head-portrait {
+    width: 80px;
+    height: 80px;
+    border-radius: 50%; 
+    object-fit: cover; 
+    border: 2px solid rgb(255, 255, 255);
+    margin-right: 15px; 
+}
+
+.name {
+    font-size: 1.2em; 
+    color: #fff; 
+}
+</style>
