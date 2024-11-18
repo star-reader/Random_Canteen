bb9af181fa077dfb4decc018dc12e28560bd81c4
star-reader <3084895416@qq.com>
2024-11-18 13:16:27 +0800

    删除废弃的模块引入

--- front_end/src/App.vue
+++ front_end/src/App.vue
@@ -10,11 +10,7 @@
 
 <script lang='ts' setup>
 import { onMounted } from 'vue';
-import axios from 'axios';
 import NavigationTabBar from './layout/NavBar/NavigationTabBar.vue';
-import router from './router';
-import { dataDecrypt } from './utils/crypto';
-import api from './config/api/api';
 import useGetToken from './hooks/useGetToken';
 
 interface LoginCert {
